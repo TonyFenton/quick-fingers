710aa07d2ab178b9a12d4810564b0cb009f66b10
Tony <tony.fenton3@gmail.com>
2018-01-12 19:44:24 +0100

    Add a replace feature

--- Test.php
+++ Test.php
@@ -22,9 +22,9 @@ class Test extends TestCase
 
     public function test_success()
     {
+        $this->createTestData(self::TEST_DIR, 'index.php__');
         $this->createTestData(self::IN_TEST_DIR.'src__', 'new.php');
         $this->createTestData(self::IN_TEST_DIR.'public__', 'new.php');
-        $this->createTestData(self::TEST_DIR, 'index.php__');
 
         $result = $this->execScript('Done, up: 3, down: 3.');
 
@@ -65,7 +65,53 @@ class Test extends TestCase
         $this->assertEquals($expected, $result);
     }
 
-    private function getDirArr(string $dir = self::TEST_DIR, array &$arr = []) // nie chce &
+    public function test_replace()
+    {
+        $this->createTestData(self::IN_TEST_DIR.'public--', 'new.php');
+        $this->createTestData(self::TEST_DIR, 'index.php--');
+
+        $result = $this->execScript('Done, up: 2, down: 2.', ['--', '--']);
+
+        $expected = [
+            self::IN_TEST_DIR.'index.php',
+            self::IN_TEST_DIR.'index.php--',
+            self::IN_TEST_DIR.'old.php',
+            self::IN_TEST_DIR.'public'.DIRECTORY_SEPARATOR.'new.php',
+            self::IN_TEST_DIR.'public--'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'src'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'templates'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'var'.DIRECTORY_SEPARATOR.'old.php',
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_revertReplace()
+    {
+        $this->createTestData(self::TEST_DIR, 'index.php_test');
+        $this->createTestData(self::IN_TEST_DIR.'src_test', 'new.php');
+        $this->createTestData(self::IN_TEST_DIR.'templates_test', 'new.php');
+        $this->createTestData(self::IN_TEST_DIR.'www_test', 'new.php');
+
+        $result = $this->execScript('Abort, reverted to the initial state. Can not rename "www".', ['_test', '_test']);
+
+        $expected = [
+            self::IN_TEST_DIR.'index.php',
+            self::IN_TEST_DIR.'index.php_test',
+            self::IN_TEST_DIR.'old.php',
+            self::IN_TEST_DIR.'public'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'src'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'src_test'.DIRECTORY_SEPARATOR.'new.php',
+            self::IN_TEST_DIR.'templates'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'templates_test'.DIRECTORY_SEPARATOR.'new.php',
+            self::IN_TEST_DIR.'var'.DIRECTORY_SEPARATOR.'old.php',
+            self::IN_TEST_DIR.'www_test'.DIRECTORY_SEPARATOR.'new.php',
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    private function getDirArr(string $dir = self::TEST_DIR, array &$arr = [])
     {
         foreach (glob($dir.DIRECTORY_SEPARATOR.'*') as $path) {
             if (is_dir($path)) {
--- quick-fingers.php
+++ quick-fingers.php
@@ -2,40 +2,38 @@
 
 $upSuffix = $argc > 1 ? $argv[1] : '__';
 $downSuffix = $argc > 2 ? $argv[2] : '_'.date('Y-m-d');
+$tmpSuffix = uniqid();
 $upCounter = 0;
 $downCounter = 0;
-$revertUp = [];
-$revertDown = [];
+$log = [];
 $msg = '';
 
 try {
     foreach (glob('*'.$upSuffix) as $element) {
         $name = substr($element, 0, -strlen($upSuffix));
-        renameElement($name, $name.$downSuffix);
-        $revertDown[] = $name;
+        renameElement($name, $name.$tmpSuffix, $log);
         $downCounter++;
-        renameElement($element, $name);
-        $revertUp[] = $name;
+        renameElement($element, $name, $log);
         $upCounter++;
+        renameElement($name.$tmpSuffix, $name.$downSuffix, $log);
     }
     $msg = sprintf('Done, up: %d, down: %d.', $upCounter, $downCounter);
 } catch (\Exception $e) {
-    foreach ($revertUp as $name) {
-        rename($name, $name.$upSuffix);
-    }
-    foreach ($revertDown as $name) {
-        rename($name.$downSuffix, $name);
+    krsort($log);
+    foreach ($log as $revert) {
+        rename($revert[1], $revert[0]);
     }
     $msg = 'Abort, reverted to the initial state. '.$e->getMessage();
 }
 
 echo $msg.PHP_EOL;
 
-function renameElement($oldname, $newname)
+function renameElement($oldname, $newname, &$log)
 {
     if (!rename($oldname, $newname)) {
         throw new \RuntimeException('Can not rename "'.$oldname.'".');
     }
+    $log[] = [$oldname, $newname];
 
     return true;
 }
