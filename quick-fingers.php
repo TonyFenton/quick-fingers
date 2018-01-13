b23fefac82a3dac6ef728142c712eb8188600f5a
Tony <tony.fenton3@gmail.com>
2018-01-13 21:49:40 +0100

    Refactor

--- quick-fingers.php
+++ quick-fingers.php
@@ -1,39 +1,92 @@
 <?php
 
-$upSuffix = $argc > 1 ? $argv[1] : '__';
-$downSuffix = $argc > 2 ? $argv[2] : '_'.date('Y-m-d');
-$tmpSuffix = uniqid();
-$upCounter = 0;
-$downCounter = 0;
-$log = [];
-$msg = '';
+$quickFingers = new QuickFingers(
+    $argc > 2 ? $argv[2] : '_'.date('Y-m-d'),
+    $argc > 1 ? $argv[1] : '__'
+);
+echo $quickFingers->exec().PHP_EOL;
 
-try {
-    foreach (glob('*'.$upSuffix) as $element) {
-        $name = substr($element, 0, -strlen($upSuffix));
-        renameElement($name, $name.$tmpSuffix, $log);
-        $downCounter++;
-        renameElement($element, $name, $log);
-        $upCounter++;
-        renameElement($name.$tmpSuffix, $name.$downSuffix, $log);
-    }
-    $msg = sprintf('Done, up: %d, down: %d.', $upCounter, $downCounter);
-} catch (\Exception $e) {
-    krsort($log);
-    foreach ($log as $revert) {
-        rename($revert[1], $revert[0]);
-    }
-    $msg = 'Abort, reverted to the initial state. '.$e->getMessage();
-}
-
-echo $msg.PHP_EOL;
-
-function renameElement($oldname, $newname, &$log)
+class QuickFingers
 {
-    if (!rename($oldname, $newname)) {
-        throw new \RuntimeException('Can not rename "'.$oldname.'".');
-    }
-    $log[] = [$oldname, $newname];
+    /** @var string */
+    private $downSuffix;
 
-    return true;
+    /** @var string */
+    private $upSuffix;
+
+    /** @var string */
+    private $tmpSuffix;
+
+    /** @var array */
+    private $renameLog = [];
+
+    /**
+     * @param string $downSuffix
+     * @param string $upSuffix
+     */
+    public function __construct($downSuffix, $upSuffix)
+    {
+        $this->downSuffix = $downSuffix;
+        $this->upSuffix = $upSuffix;
+        $this->tmpSuffix = uniqid();
+    }
+
+    /**
+     * @return string
+     */
+    public function exec()
+    {
+        try {
+            $msg = $this->run();
+        } catch (\Exception $e) {
+            $msg = $this->revert($e->getMessage());
+        }
+
+        return $msg;
+    }
+
+    /**
+     * @return string
+     */
+    private function run()
+    {
+        $counter = ['up' => 0, 'down' => 0];
+        foreach (glob('*'.$this->upSuffix) as $element) {
+            $name = substr($element, 0, -strlen($this->upSuffix));
+            $this->rename($name, $name.$this->tmpSuffix);
+            $counter['down']++;
+            $this->rename($element, $name);
+            $counter['up']++;
+            $this->rename($name.$this->tmpSuffix, $name.$this->downSuffix);
+        }
+
+        return sprintf('Done, up: %d, down: %d.', $counter['up'], $counter['down']);
+    }
+
+    /**
+     * @param string $errorMsg
+     * @return string
+     */
+    private function revert($errorMsg)
+    {
+        krsort($this->renameLog);
+        foreach ($this->renameLog as $rename) {
+            rename($rename[1], $rename[0]);
+        }
+        $this->renameLog = [];
+
+        return 'Abort, reverted to the initial state. '.$errorMsg;
+    }
+
+    /**
+     * @param string $oldname
+     * @param string $newname
+     */
+    private function rename($oldname, $newname)
+    {
+        if (!rename($oldname, $newname)) {
+            throw new \RuntimeException('Can not rename "'.$oldname.'".');
+        }
+        $this->renameLog[] = [$oldname, $newname];
+    }
 }
